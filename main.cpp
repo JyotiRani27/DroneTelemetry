8dbc192dcafcdfdcb7b666970b7eb4257a34d2ed
Jyoti Rani <jyotinetyashree@gmail.com>
2025-07-01 21:29:50 +0530

    header correction

--- main.cpp
+++ main.cpp
@@ -1,6 +1,6 @@
 // ======================= main.cpp =======================
 #include <QApplication>
-#include "mainWindows.h"
+#include "MainWindow.h"
 
 int main(int argc, char *argv[]) {
     QApplication app(argc, argv);
